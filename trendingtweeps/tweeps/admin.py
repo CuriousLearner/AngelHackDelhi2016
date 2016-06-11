7fb8c4a4acc1f16cf0d4c6f46c270aff177fda6c
CuriousLearner <sanyam.khurana01@gmail.com>
2016-06-11 20:31:44 +0530

    Admin panel for twitter users

--- trendingtweeps/tweeps/admin.py
+++ trendingtweeps/tweeps/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import TwitterUser
+
 # Register your models here.
+class TwitterUserAdmin(admin.ModelAdmin):
+    list_display = ('twitter_id', 'follower_count', 'total_fav_count', 'total_retweet_count', 'location')
+
+admin.site.register(TwitterUser, TwitterUserAdmin)
--- trendingtweeps/tweeps/models.py
+++ trendingtweeps/tweeps/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class TwitterUser(models.Model):
+	twitter_id = models.CharField(max_length=20)
+	follower_count = models.PositiveIntegerField()
+	total_fav_count = models.PositiveIntegerField()
+	total_retweet_count = models.PositiveIntegerField()
+	location = models.CharField(max_length=30)
