47447324452a3ffc947d1170015b6c4c4328f0f0
Nikhil Handa <nikhilhanda@live.com>
2016-06-12 05:24:38 +0530

    Front end rev1

---
+++ trendingtweeps/tweeps/static/js/script.js
@@ -0,0 +1,275 @@
+var cardTemplate=$("#card-template").html();
+var message = [
+
+    {
+        "total_fav_count": 4,
+        "impact_score": 0.2809361438313701,
+        "total_retweet_count": 6,
+        "twitter_id": "322579876",
+        "twitter_username": "CMYK_Bookstore",
+        "location": "Delhi",
+        "follower_count": 482
+    }, {
+        "total_fav_count": 1,
+        "impact_score": 1.0245040297582144,
+        "total_retweet_count": 1,
+        "twitter_id": "41448609",
+        "twitter_username": "72interactive",
+        "location": "Delhi",
+        "follower_count": 1785
+    }, {
+        "total_fav_count": 1,
+        "impact_score": 0.07704587724736515,
+        "total_retweet_count": 3,
+        "twitter_id": "3901029193",
+        "twitter_username": "YesMinisterDEL",
+        "location": "Delhi",
+        "follower_count": 131
+    }, {
+        "total_fav_count": 17,
+        "impact_score": 0.17695288282703037,
+        "total_retweet_count": 93,
+        "twitter_id": "1535872880",
+        "twitter_username": "CraftsMuseum",
+        "location": "Delhi",
+        "follower_count": 213
+    }, {
+        "total_fav_count": 3,
+        "impact_score": 0.35657160570365776,
+        "total_retweet_count": 0,
+        "twitter_id": "338207479",
+        "twitter_username": "KhushiiNGO",
+        "location": "Delhi",
+        "follower_count": 620
+    }, {
+        "total_fav_count": 0,
+        "impact_score": 0.4359268443893366,
+        "total_retweet_count": 35,
+        "twitter_id": "300139955",
+        "twitter_username": "HCFIIndia",
+        "location": "Delhi",
+        "follower_count": 728
+    }, {
+        "total_fav_count": 9,
+        "impact_score": 0.23750774953502787,
+        "total_retweet_count": 27,
+        "twitter_id": "433254606",
+        "twitter_username": "Area79Festival",
+        "location": "Delhi",
+        "follower_count": 384
+    }, {
+        "total_fav_count": 4,
+        "impact_score": 0.704959702417855,
+        "total_retweet_count": 1,
+        "twitter_id": "523100450",
+        "twitter_username": "webexpertzin",
+        "location": "Delhi",
+        "follower_count": 1226
+    }, {
+        "total_fav_count": 0,
+        "impact_score": 0.38130812151270926,
+        "total_retweet_count": 1,
+        "twitter_id": "1871174502",
+        "twitter_username": "monkeybardel",
+        "location": "Delhi",
+        "follower_count": 664
+    }, {
+        "total_fav_count": 5,
+        "impact_score": 0.6267823930564167,
+        "total_retweet_count": 0,
+        "twitter_id": "1229742769",
+        "twitter_username": "Neeraj_Lavaniya",
+        "location": "Delhi",
+        "follower_count": 1090
+    }, {
+        "total_fav_count": 2,
+        "impact_score": 0.7495815251084935,
+        "total_retweet_count": 1,
+        "twitter_id": "1922443320",
+        "twitter_username": "WebDesignSUMO",
+        "location": "Delhi",
+        "follower_count": 1305
+    }, {
+        "total_fav_count": 0,
+        "impact_score": 0.060725356478611284,
+        "total_retweet_count": 63,
+        "twitter_id": "262609360",
+        "twitter_username": "harishri_India",
+        "location": "Delhi",
+        "follower_count": 48
+    }, {
+        "total_fav_count": 4,
+        "impact_score": 0.1911810291382517,
+        "total_retweet_count": 0,
+        "twitter_id": "49670842",
+        "twitter_username": "Paharpur",
+        "location": "Delhi",
+        "follower_count": 331
+    }, {
+        "total_fav_count": 34,
+        "impact_score": 3.186670799752015,
+        "total_retweet_count": 29,
+        "twitter_id": "71776841",
+        "twitter_username": "SelectCITYWALK",
+        "location": "Delhi",
+        "follower_count": 5510
+    }, {
+        "total_fav_count": 63,
+        "impact_score": 5.809345939243645,
+        "total_retweet_count": 15,
+        "twitter_id": "167632572",
+        "twitter_username": "JobsAtGates",
+        "location": "Delhi",
+        "follower_count": 10079
+    }, {
+        "total_fav_count": 7,
+        "impact_score": 0.506633601983881,
+        "total_retweet_count": 21,
+        "twitter_id": "505759060",
+        "twitter_username": "WestinGurgaonND",
+        "location": "Delhi",
+        "follower_count": 860
+    }, {
+        "total_fav_count": 37,
+        "impact_score": 0.6111748295102294,
+        "total_retweet_count": 77,
+        "twitter_id": "1142153167",
+        "twitter_username": "jiviteshhotel",
+        "location": "Delhi",
+        "follower_count": 973
+    }, {
+        "total_fav_count": 1,
+        "impact_score": 0.23546187228766274,
+        "total_retweet_count": 0,
+        "twitter_id": "259749007",
+        "twitter_username": "cprohini",
+        "location": "Delhi",
+        "follower_count": 410
+    }, {
+        "total_fav_count": 15,
+        "impact_score": 0.31818040917544943,
+        "total_retweet_count": 1,
+        "twitter_id": "2245532845",
+        "twitter_username": "eroshotelnd",
+        "location": "Delhi",
+        "follower_count": 545
+    }, {
+        "total_fav_count": 4,
+        "impact_score": 2.176208927464352,
+        "total_retweet_count": 130,
+        "twitter_id": "2156462822",
+        "twitter_username": "WeMark_India",
+        "location": "Delhi",
+        "follower_count": 3673
+    }, {
+        "total_fav_count": 0,
+        "impact_score": 0.01032238065716057,
+        "total_retweet_count": 0,
+        "twitter_id": "2766103682",
+        "twitter_username": "LiquorShopDelhi",
+        "location": "Delhi",
+        "follower_count": 18
+    }, {
+        "total_fav_count": 2,
+        "impact_score": 0.4799597024178549,
+        "total_retweet_count": 3,
+        "twitter_id": "140086004",
+        "twitter_username": "RadialHR",
+        "location": "Delhi",
+        "follower_count": 833
+    }, {
+        "total_fav_count": 20,
+        "impact_score": 1.298171109733416,
+        "total_retweet_count": 2,
+        "twitter_id": "78870857",
+        "twitter_username": "induswebi",
+        "location": "Delhi",
+        "follower_count": 2250
+    }, {
+        "total_fav_count": 2,
+        "impact_score": 5.308880967141972,
+        "total_retweet_count": 9073,
+        "twitter_id": "154526188",
+        "twitter_username": "pranitarottmann",
+        "location": "Delhi",
+        "follower_count": 919
+    }, {
+        "total_fav_count": 4,
+        "impact_score": 0.887569745815251,
+        "total_retweet_count": 933,
+        "twitter_id": "44605684",
+        "twitter_username": "ErSanyamKhurana",
+        "location": "New Delhi",
+        "follower_count": 688
+    }, {
+        "total_fav_count": 6,
+        "impact_score": 0.14747365158090514,
+        "total_retweet_count": 5,
+        "twitter_id": "447875983",
+        "twitter_username": "slayslot",
+        "location": "New Delhi, India ",
+        "follower_count": 249
+    }, {
+        "total_fav_count": 5,
+        "impact_score": 0.39032858028518286,
+        "total_retweet_count": 300,
+        "twitter_id": "15089974",
+        "twitter_username": "st",
+        "location": "europe",
+        "follower_count": 402
+    }
+
+]
+$(document).ready(function() {
+    $('#handle').keypress(function(e) {
+        var key = e.which;
+        if (key == 13) // the enter key code
+        {
+            var text = $(this).val();
+            $(this).val('');
+            //requestData(text);
+            handleSearch(message, text);
+        }
+    });
+    $('#handleSubmit').click(function() {
+        var text = $('#handle').val();
+        $('#handle').val('');
+        //requestData(text);
+        handleSearch(message, text);
+    });
+
+    // function requestData(handlename) {
+    //     var request = $.ajax({
+    //         url: "http://stormy-gorge-8134.herokuapp.com/getimpact/"+handlename+"/,
+    //         method: "GET",
+    //         dataType: "json"
+    //     });
+    //     request.done(function(msg) {
+    //         if (msg.length == 0) {
+    //             alert('Nothing here');
+    //             $('<h2 class="text-center">Houston, we have nothing here.<b>Stay tuned.</b></h2>').appendTo('.single-event');
+    //         } else {
+    //             handleSearch(msg,handlename);
+    //         }
+    //     });
+    //
+    //     request.fail(function(jqXHR, textStatus) {
+    //         $('<h2 class="text-center">Houston, our server has faced some unexpected error.<b>Stay tuned.</b></h2>').appendTo('.single-event');
+    //         console.log("Request failed: " + textStatus);
+    //     });
+    // }
+    function handleSearch(message, handlename) {
+        $('.form-wrapper').hide();
+        $.each(message, function(i, e) {
+            if (message[i].twitter_username == handlename) {
+                var adddata= cardTemplate.replace('userhandle">','userhandle">'+message[i].twitter_username);
+                adddata= adddata.replace('location">','location">'+message[i].location);
+                adddata= adddata.replace('impactScore">','impactScore">'+message[i].impact_score);
+                adddata=adddata.replace('favorites">','favorites">'+message[i].total_fav_count);
+                adddata=adddata.replace('rts">','rts">'+message[i].total_retweet_count);
+                adddata=adddata.replace('followers">','followers">'+message[i].follower_count);
+                $(adddata).appendTo('.dynamic');
+            }
+        });
+    }
+});
